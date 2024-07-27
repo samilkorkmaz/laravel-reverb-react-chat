7f51d21c63462fc86ddc1df5461da2ba65561fac
Şamil Korkmaz <samil.korkmaz@gmail.com>
2024-07-27 12:35:26 +0300

    Only show messages whose to_id matches userId

---
+++ database/migrations/2024_07_27_092700_add_to_id_to_messages_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('messages', function (Blueprint $table) {
+            $table->unsignedBigInteger('to_id')->after('user_id');
+            $table->foreign('to_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('messages', function (Blueprint $table) {
+            $table->dropForeign(['to_id']);
+            $table->dropColumn('to_id');
+        });
+    }
+};
+
--- resources/js/components/Message.jsx
+++ resources/js/components/Message.jsx
@@ -1,14 +1,24 @@
-
-import React from "react";
+import React, {useEffect} from "react";
 
 const Message = ({ userId, message }) => {
+
+    useEffect(() => {
+        // Log message to the console
+        console.log("message", message);
+    }, [message]);
+
+    // Only render the message if the to_id matches the userId
+    if (message.to_id !== userId) {
+        return null;
+    }
+
     return (
         <div className={`row ${userId === message.user_id ? "justify-content-end" : ""}`}>
             <div className="col-md-6">
                 <small className="text-muted float-right">
                     {message.time}
                 </small>
-                <small className="text-muted">{message.name} - user ID: {message.user_id}</small>
+                <small className="text-muted">{message.name} - user ID: {message.user_id} - name: {message.user.name}</small>
                 <div className={`alert alert-${userId === message.user_id ? "primary" : "secondary"}`} role="alert">
                     {message.text}
                 </div>
