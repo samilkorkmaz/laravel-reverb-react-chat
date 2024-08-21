c68f13bcdc65340643a4bc92020e03ccca2998a8
samil2 <samil.korkmaz@gmail.com>
2024-08-21 10:39:45 +0300

    Fix userName problems

--- resources/js/components/ChatBox.jsx
+++ resources/js/components/ChatBox.jsx
@@ -3,34 +3,32 @@ import Message from "./Message.jsx";
 import MessageInput from "./MessageInput.jsx";
 
 const ChatBox = ({ rootUrl }) => {
-    console.log("ChatBox");
+    //console.log("ChatBox");
     const mainElement = document.getElementById('main');
 
     // Extract user data from the DOM element
     const userData = mainElement.getAttribute('data-user');
     const user = JSON.parse(userData);
-    console.log(user);
 
     // Extract messages data from the DOM element
     const messagesData = mainElement.getAttribute('data-messages');
     const initialMessages = JSON.parse(messagesData);
-    console.log(initialMessages);
 
     const webSocketChannel = `App.Models.User.${user.id}`;
     //const webSocketChannel = `channel_for_everyone`;
 
     const [messages, setMessages] = useState(initialMessages); // Initialize with messages passed from the view
     const scroll = useRef();
+    //console.log(user);
+    //console.log(messages);
 
-    console.log("1");
     const scrollToBottom = () => {
-        console.log("scrollToBottom");
+        //console.log("scrollToBottom");
         scroll.current.scrollIntoView({ behavior: "smooth" });
     };
-    console.log("2");
 
     const connectWebSocket = () => {
-        console.log("connectWebSocket");
+        //console.log("connectWebSocket");
         window.Echo.private(webSocketChannel)
             .listen('GotMessage', (e) => {
                 // Append the new message to the state
@@ -38,10 +36,9 @@ const ChatBox = ({ rootUrl }) => {
                 setTimeout(scrollToBottom, 0);
             });
     }
-    console.log("3");
 
     useEffect(() => {
-        console.log("useEffect");
+        //console.log("useEffect");
         connectWebSocket();
         scrollToBottom();
 
@@ -49,7 +46,6 @@ const ChatBox = ({ rootUrl }) => {
             window.Echo.leave(webSocketChannel);
         }
     }, []);
-    console.log("4");
 
     return (
         <div className="row justify-content-center">
@@ -57,12 +53,17 @@ const ChatBox = ({ rootUrl }) => {
                 <div className="card">
                     <div className="card-header">Chat Box - {user.name}</div>
                     <div className="card-body" style={{height: "500px", overflowY: "auto"}}>
-                        {messages?.map((message) => (
-                            <Message key={message.id}
-                                     userId={user.id}
-                                     message={message}
-                            />
-                        ))}
+                        {messages?.map((message, index) => {
+                            //console.log(`Rendering message ${index + 1}:`, message);
+                            //console.log(`user.name: ${user.name}`);
+                            return (
+                                <Message key={message.id}
+                                         userId={user.id}
+                                         userName={user.name}
+                                         message={message}
+                                />
+                            );
+                        })}
                         <span ref={scroll}></span>
                     </div>
                     <div className="card-footer">
--- resources/js/components/Message.jsx
+++ resources/js/components/Message.jsx
@@ -1,10 +1,9 @@
 import React, { useEffect } from "react";
 
-const Message = ({ userId, message }) => {
-
+const Message = ({ userId, userName, message }) => {
     useEffect(() => {
         // Log message to the console
-        console.log("message", message);
+        //console.log("message", message);
     }, [message]);
 
     // Only render the message if the user_id or to_id matches the userId
@@ -12,16 +11,25 @@ const Message = ({ userId, message }) => {
         return null;
     }
 
+    const date = new Date(message.updated_at);
+    // Extract the date and time components
+    const formattedDate = date.toISOString().split('T')[0];
+    const formattedTime = date.toTimeString().split(' ')[0];
+    // Combine them into the desired format
+    const shortTime = `${formattedDate} ${formattedTime}`;
+
     return (
         <div className={`row ${userId === message.user_id ? "justify-content-end" : ""}`}>
             <div className="col-md-6">
                 <small className="text-muted float-right">
-                    {message.time}
+                    {shortTime}
                 </small>
                 {userId === message.user_id ? (
-                    <small className="text-muted"> - to {message.to_id}</small>
+                    <small className="text-muted"> - From {userName} to {message.to_id}</small>
                 ) : (
-                    <small className="text-muted"> - {message.user.name} ({message.user_id})</small>
+                    <small className="text-muted">
+                        - From {message.user_id} to {userName}
+                    </small>
                 )}
 
                 <div className={`alert alert-${userId === message.user_id ? "primary" : "secondary"}`} role="alert">
