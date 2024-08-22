dac82659683cf8851cfdc4b0d6d1780d2a741243
samil2 <samil.korkmaz@gmail.com>
2024-08-22 09:42:08 +0300

    Make it work with both 127.0.0.1 and localhost

--- resources/js/components/Main.jsx
+++ resources/js/components/Main.jsx
@@ -1,11 +1,13 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '../../css/app.css';
 import ChatBox from "./ChatBox.jsx";
 
 if (document.getElementById('main')) {
-    const rootUrl = "http://127.0.0.1:8000";
+    const hostname = window.location.hostname;
+    const rootUrl = hostname === "127.0.0.1" || hostname === "localhost"
+        ? `http://${hostname}:8000`
+        : "http://127.0.0.1:8000"; // fallback to 127.0.0.1 if hostname is neither
 
     ReactDOM.createRoot(document.getElementById('main')).render(
         <React.StrictMode>
