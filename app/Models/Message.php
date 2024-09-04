a8b0a1cee578eb41745845979cf36e9bd0f29a1c
Şamil Korkmaz <samil.korkmaz@gmail.com>
2024-09-04 22:42:27 +0300

    Remove HasFactory

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -2,15 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Facades\Log;
 
 class Message extends Model
 {
-    use HasFactory;
-
     public $table = 'messages';
 
     protected $fillable = [
@@ -29,9 +26,14 @@ class Message extends Model
         return $this->belongsTo(User::class, 'to_id');
     }
 
-    //getTimeAttribute is a Laravel accessor. It is automatically invoked when you try to access the time attribute on a Message model instance (e.g., $message->time).
+    /**
+     * Get the formatted time attribute.
+     * getTimeAttribute is a Laravel accessor. It is automatically invoked when you try to access the time attribute on a Message model instance (e.g. message->time).
+     *
+     * @return string
+     */
     public function getTimeAttribute(): string
     {
-        return date("d M Y, H:i:s", strtotime($this->attributes['created_at']));
+        return date("d M Y, H:i:s", strtotime($this->created_at));
     }
 }
